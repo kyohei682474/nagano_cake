3e7ead7524e04e55047729d5f7c5f744143b5000
EC2 Default User <ec2-user@ip-172-31-24-18.ap-northeast-1.compute.internal>
2023-04-19 10:32:04 +0000

    edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,11 @@
 class Public::CustomersController < ApplicationController
     def show
-          @customer = current_customer
+        @customer = current_customer
 
     end
 
     def edit
+        @customer = current_customer
     end
 
     def update
@@ -14,6 +15,11 @@ class Public::CustomersController < ApplicationController
     end
 
     def withdrawal
+    end
+
+  private
+    def current_params
+        params.require(:current).permit(:last_name, :first_name, :last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
     end
 
 end
