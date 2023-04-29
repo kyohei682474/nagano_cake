c7e09fb70e95c2b0bf8a035a9d4a3fd4b1ddef37
EC2 Default User <ec2-user@ip-172-31-24-18.ap-northeast-1.compute.internal>
2023-04-29 09:20:12 +0000

    make cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,30 @@
 class Public::CartItemsController < ApplicationController
     def index
-        @cart_items = Cart_item.all
+        @cart_items = CartItem.all
+
     end
-    
+
     def update
+
     end
-    
+
     def show
     end
-    
+
     def create
+        cart_item = CartItem.new(cart_item_params)
+        cart_item.save
+        redirect_to cart_items_path
     end
-   
+
     def destroy
     end
-    
+
     def destroy_all
     end
+
+    def cart_item_params
+     params.require(:cart_item).permit(:item_id,:customer_id,:amount)
+    end
+
 end
