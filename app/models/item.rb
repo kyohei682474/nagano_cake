3e9acab5b252b1cfc7d8bc950ee228eb6c573271
EC2 Default User <ec2-user@ip-172-31-24-18.ap-northeast-1.compute.internal>
2023-04-30 14:10:28 +0000

    asosiation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     has_one_attached :image
     has_many :cart_item, dependent: :destroy
+    has_many :order_item, dependent: :destroy
     belongs_to :genre
 
     def with_tax_price
