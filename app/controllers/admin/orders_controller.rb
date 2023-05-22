11494a17c1a4f673f998bbe0dddc6ddf9f59d6ca
EC2 Default User <ec2-user@ip-172-31-24-18.ap-northeast-1.compute.internal>
2023-05-22 12:51:00 +0000

    test2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,9 +8,12 @@ class Admin::OrdersController < ApplicationController
 
     def update
         order = Order.find(params[:id])
+        order_details = OrderDetail.where(order_id: params[:id])
         # order_detail = order.order_details
-        order.update(order_params)
+     if order.update(order_params)
+        order_details.update_all(making_status: 1) if order.status == "payment_confirmation"
         # order_detail.update(order_detail)
+     end
         redirect_to admin_order_path(order.id)
 
     end
@@ -19,8 +22,5 @@ class Admin::OrdersController < ApplicationController
         params.require(:order).permit(:customer_id, :status, :payment_method,:amount_billed,:shipping_fee,:delivery_target_postal_code,:delivery_address,:delivery_target_full_name,:created_at,:updated_at,:making_status)
     end
 
-    def order_detail_params
-        params.require(:order_detail).permit(:making_status)
-    end
 
 end
