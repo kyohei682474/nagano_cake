6394d40c7f5bea82bae9ba30173c3b3c6bb1b8cd
EC2 Default User <ec2-user@ip-172-31-24-18.ap-northeast-1.compute.internal>
2023-05-24 04:39:39 +0000

    注文ステータスと制作ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -6,25 +6,30 @@ class Admin::OrderDetailsController < ApplicationController
         order = Order.find(order_detail.order_id)
         order_details = order.order_details.all
 
-
-
-
         order_detail.update(order_detail_params)
+
         if
            order_detail.making_status == "production"
            order.update(status:2)
-        elsif
-           order_details.each do |order_detail|
-           order_detail.making_status = "production_completed"
-           end
-           order.update(status:3)
-        end
+
+        else
+           change_status(order_details,order)
+         end
         redirect_to admin_order_path(order_detail.order.id)
     end
 
 
 
 private
+
+     def change_status(order_details,order)
+          order_details.each do |order_detail|
+           if order_detail.making_status != "production_completed"
+              return
+           end
+           end
+           order.update(status:3)
+     end
      def order_detail_params
         params.require(:order_detail).permit(:making_status)
      end
